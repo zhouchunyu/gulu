c58b173e9ba837d9ef75695cecefa75a93617f20
zhouchunyu <zhouchunyu20xx@163.com>
2018-07-27 10:57:14 +0800

    use parcel

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="g-button">hi</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="sass">
+    .g-button {
+        height: var(--button-height);
+        font-size: var(--font-size);
+        padding: 0 var(--button-padding);
+        background: var(--button-bg);
+        border-radius:  var(--border-radius);
+        color: var(--button-color);
+        border: 1px solid var(--border-color);
+        &:hover {
+            border: 1px solid var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+
+    }
+</style>
