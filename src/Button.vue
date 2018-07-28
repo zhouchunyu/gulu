ce0e3de8cb7795c25fc88d95fdfb09f4618c64c5
zhouchunyu <zhouchunyu20xx@163.com>
2018-07-28 16:00:43 +0800

    改为scss

--- src/Button.vue
+++ src/Button.vue
@@ -2,20 +2,26 @@
     <button class="g-button" :class="[`icon-${iconPosition}`]" @click="$emit('click')">
         <g-icon v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon v-if="icon && loading" :name="icon" class="loading"></g-icon>
-        <div class="content"><slot></slot></div>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 </template>
 
 <script>
     export default {
-        props: {'icon': {}, 'iconPosition': {type: String, default: 'left', validator(value) {
-            return !(value !== 'left' && value !== 'right')
-        }}, 'loading': {type: Boolean, default: false}}
+        props: {
+            'icon': {}, 'iconPosition': {
+                type: String, default: 'left', validator(value) {
+                    return !(value !== 'left' && value !== 'right')
+                }
+            }, 'loading': {type: Boolean, default: false}
+        }
 
     }
 </script>
 
-<style lang="sass">
+<style lang="scss">
     .g-button {
         height: var(--button-height);
         font-size: var(--font-size);
@@ -39,13 +45,13 @@
         }
         .loading {
             animation: spin 1.5s infinite linear;
+        }
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
             }
-                @keyframes spin {
-                0% {
-                transform:rotate(0deg);
-            }
-                100% {
-                transform:rotate(360deg);
+            100% {
+                transform: rotate(360deg);
             }
         }
 
@@ -57,8 +63,14 @@
             margin-right: .3em;
         }
         &.icon-right {
-            > .content { order: 1;}
-            > .icon { order: 2; margin-right: 0; margin-left: .3em;}
+            > .content {
+                order: 1;
+            }
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
         }
     }
 </style>
