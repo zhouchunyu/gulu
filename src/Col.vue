8c464d3aecc71a8a0cb182e2ab85387c3baeff18
zhouchunyu <zhouchunyu20xx@163.com>
2018-07-30 19:33:26 +0800

    增加布局组件

---
+++ src/Col.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            'span': {
+                type: [Number, String]
+            },
+            'offset': {
+                type: [Number, String]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .col{
+        height: 100px;
+        background: grey;
+        border: 1px solid red;
+        width: 50%;
+    }
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+            width: ($n / 24) * 100%;
+        }
+    }
+    $offset-prefix: offset-;
+    @for $n from 1 through 24 {
+        &.#{$offset-prefix}#{$n} {
+            margin-left: ($n / 24) * 100%;
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import Vue from 'vue';
 
 import Button from './Button';
 import Icon from './Icon';
-import Input from './Input'
+import Input from './Input';
+import Col from './Col';
+import Row from './Row';
+
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-input', Input);
+Vue.component('g-col', Col);
+Vue.component('g-row', Row);
+
+
 
 new Vue({
     el: '#app',
